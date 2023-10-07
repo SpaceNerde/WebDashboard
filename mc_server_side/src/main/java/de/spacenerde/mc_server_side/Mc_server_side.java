c8272f408b8de99d07cfe31766953430d9f56cef
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-10-07 12:43:43 +0200

    Basic Setup

---
+++ mc_server_side/src/main/java/de/spacenerde/mc_server_side/Mc_server_side.java
@@ -0,0 +1,17 @@
+package de.spacenerde.mc_server_side;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mc_server_side extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
